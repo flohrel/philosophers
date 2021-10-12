366801325f8df63da2f909616cd94f1b42aa66f3
flohrel <flohrel@student.42.fr>
2021-10-12 18:16:43 +0200

    parsing OK

--- philo/incld/philo.h
+++ philo/incld/philo.h
@@ -6,13 +6,59 @@
 /*   By: flohrel <flohrel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 16:44:30 by flohrel           #+#    #+#             */
-/*   Updated: 2021/10/11 18:15:33 by flohrel          ###   ########.fr       */
+/*   Updated: 2021/10/12 17:58:30 by flohrel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+/*
+**		LIB
+*/
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <limits.h>
 
+/*
+**		MACRO
+*/
+# define THREADS_MAX	4096
+
+/*
+**		TYPE
+*/
+typedef struct s_philo	t_philo;
+typedef struct s_param	t_param;
+typedef struct s_vars	t_vars;
+
+struct	s_philo
+{
+	int				id;
+	pthread_mutex_t	fork;
+	t_philo			*next;
+};
+
+struct	s_param
+{
+	int	nb_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	nb_eat;
+};
+
+struct	s_vars
+{
+	t_param	param;
+	t_philo	*philo;
+};
+
+/*
+**		FUNCTION
+*/
+int	parse_args(int argc, char **argv, t_param *param);
 
 #endif
--- philo/src/main.c
+++ philo/src/main.c
@@ -6,13 +6,34 @@
 /*   By: flohrel <flohrel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/11 18:01:42 by flohrel           #+#    #+#             */
-/*   Updated: 2021/10/11 18:08:26 by flohrel          ###   ########.fr       */
+/*   Updated: 2021/10/12 17:26:28 by flohrel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+int	clean_exit(int ret_val, t_vars *vars)
+{
+	(void)vars;
+	return (ret_val);
+}
+
+void	init_vars(t_vars *vars, t_param *param)
+{
+	(void)vars;
+	param->nb_philo = -1;
+	param->time_to_die = -1;
+	param->time_to_eat = -1;
+	param->time_to_sleep = -1;
+	param->nb_eat = -1;
+}
+
 int	main(int argc, char **argv)
 {
+	t_vars	vars;
+
+	init_vars(&vars, &vars.param);
+	if (parse_args(argc, argv, &vars.param) == -1)
+		return (EXIT_FAILURE);
 	return (0);
 }
