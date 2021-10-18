45c5a49ff14f4793d77ed15c33fd60a96cf20f9c
flohrel <flohrel@student.42.fr>
2021-10-18 13:54:28 +0200

    probleme timing a +- 10 ms

--- philo/src/waiter.c
+++ philo/src/waiter.c
@@ -6,20 +6,20 @@
 /*   By: flohrel <flohrel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 17:49:43 by flohrel           #+#    #+#             */
-/*   Updated: 2021/10/18 10:31:02 by flohrel          ###   ########.fr       */
+/*   Updated: 2021/10/18 13:51:33 by flohrel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*observe(void *arg)
+void	waiter(t_vars *vars)
 {
-	t_vars		*vars;
+//	t_vars		*vars;
 	t_param		*param;
 	t_philo		*philo;
 	int64_t		timestamp;
 
-	vars = arg;
+//	vars = arg;
 	param = &vars->param;
 	philo = vars->table;
 	while (1)
@@ -27,7 +27,6 @@ void	*observe(void *arg)
 		timestamp = get_ms_time() - param->start_time;
 		if ((timestamp - philo->last_meal) > param->time_to_die)
 		{
-			printf("timestamp:%ld last:%ld\n", timestamp, philo->last_meal);
 			timestamp_msg(philo->id, "died", param->start_time);
 			param->has_ended = true;
 			break ;
@@ -36,13 +35,12 @@ void	*observe(void *arg)
 			break ;
 		philo = philo->next;
 	}
-	return (NULL);
 }
 
-void	waiter(t_vars *vars)
+/*void	waiter(t_vars *vars)
 {
 	pthread_t	thread_id;
 
 	pthread_create(&thread_id, NULL, observe, vars);
 	pthread_join(thread_id, NULL);
-}
+}*/
